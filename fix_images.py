aecfcbfc70b3ee98c73201b4e41287ff8b0b082e
ramossinflores <mlaura.ragra@gmail.com>
2025-06-24 21:37:11 +0100

    feat: Script para evaluar el uso de la librería pillow - problem_statement.md contiene el enunciado de la práctica a resolver en el QWiklabs - Agregado requeriments - Se ignoran los directorios y archivos usados para las pruebas - La lógica está toda englobada en fix_images.py

---
+++ fix_images.py
@@ -0,0 +1,37 @@
+
+import os
+from PIL import Image
+
+# Directorio de las fotos
+directory = "./photos"
+directory_new = f"{directory}_fixed"
+
+# Crea el directorio donde se guardaran las fotos , si no existe 
+try: 
+    if not os.path.exists(directory_new):
+        os.makedir(directory_new)
+except Exception as e:
+    print(f"Error al crear el directorio: {e}")
+
+# Listando las fotos  en el directorio
+photos=os.listdir(directory)
+
+for photo in photos:
+    try:
+        # Separando y guardando el nombre y la extensión de cada foto
+        photo_name,photo_extension = os.path.splitext(photo)
+        # crear un objeto imagen 
+        photo = Image.open(f"./{directory}/{photo}")
+        # rotando la foto
+        photo.rotate(-90)
+        # redimensionando
+        size = (1200,900) # tamaño de prueba
+        photo.thumbnail(size)
+        # Guardar la imagen con el cambio de extensión 
+        photo.save(f"{directory_new}/{photo_name}.png")
+    except Exception as e:
+        print(f"Error al abrir la imagen {e}")
+
+    # Fuentes: 
+    # https://www.toolify.ai/es/ai-news-es/manipulacin-de-imgenes-en-python-con-pillow-1149800#:~:text=Una%20vez%20que%20tenemos%20las,"from%20PIL%20import%20Image".
+    # https://www.freecodecamp.org/espanol/news/como-crear-un-directorio-con-python/
---
+++ problem_statement.md
@@ -0,0 +1,6 @@
+
+> Para completar este módulo, tendrás que escribir un script que procese un montón de imágenes. Resulta que su empresa está actualizando su sitio web y ha contratado a un diseñador para que cree nuevos iconos gráficos para el sitio. Sin embargo, el contratista ha entregado los diseños finales y están en un formato incorrecto, girados 90° y demasiado grandes. No puedes ponerte en contacto con los diseñadores y tu propio plazo de entrega se acerca rápidamente. Tendrás que utilizar Python para que las imágenes estén listas para el lanzamiento. ¿Cómo lo harás? Tendrás que revisar una carpeta llena de imágenes y operar con cada una de ellas. En cada imagen, usarás métodos PIL como los que vimos en los ejemplos, y luego escribirás las nuevas imágenes en el lugar correcto.
+
+1. abrir cada archivo
+2. quitar el giro
+3. hacer que tengan un tamaño adecuado
