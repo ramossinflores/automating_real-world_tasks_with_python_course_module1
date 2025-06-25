b566ceb728dde2681de217091a267705e0b520b6
ramossinflores <mlaura.ragra@gmail.com>
2025-06-25 09:04:55 +0100

    feat: Convertir a rgb para evitar errores al guardar en otros formatos de imágenes

--- fix_images.py
+++ fix_images.py
@@ -21,17 +21,21 @@ for photo in photos:
         # Separando y guardando el nombre y la extensión de cada foto
         photo_name,photo_extension = os.path.splitext(photo)
         # crear un objeto imagen 
-        photo = Image.open(f"./{directory}/{photo}")
-        # rotando la foto
-        photo.rotate(-90)
-        # redimensionando
-        size = (1200,900) # tamaño de prueba
-        photo.thumbnail(size)
-        # Guardar la imagen con el cambio de extensión 
-        photo.save(f"{directory_new}/{photo_name}.png")
+        with Image.open(f"./{directory}/{photo}") as photo:
+            # convertir a rgb para evitar errores al guardar en otros formatos de imágenes
+            photo = photo.convert("RGB")
+            # rotando la foto
+            photo.rotate(-90)
+            # redimensionando
+            size = (1200,900) # tamaño de prueba
+            photo.thumbnail(size)
+            # Guardar la imagen con el cambio de extensión 
+            photo.save(f"{directory_new}/{photo_name}.png")
     except Exception as e:
         print(f"Error al procesar la imagen {e}")
 
     # Fuentes: 
     # https://www.toolify.ai/es/ai-news-es/manipulacin-de-imgenes-en-python-con-pillow-1149800#:~:text=Una%20vez%20que%20tenemos%20las,"from%20PIL%20import%20Image".
     # https://www.freecodecamp.org/espanol/news/como-crear-un-directorio-con-python/
+
+    
